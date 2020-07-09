627927ab488dc692697b0873d64b6088ff13de6d
Hamilton <hamihomeme@gmail.com>
2020-07-09 13:06:58 -0700

    home work

---
+++ PyBank/main.py
@@ -0,0 +1,28 @@
+
+
+# Path to collect data from Resources folder
+import os
+import csv 
+
+budget_data.csv = os.path.join('..', 'analysis', 'Resources', 'budget_data.csv')
+#budget_data =csv.DictReader(budget_data)
+#for row in budget_data:
+    #lsprint(row['Month'], row['Profit/Los'])
+
+# The total number of months
+
+
+# Net total of "profit and Loss" entire period
+
+
+# Average of changes in profit and loss over entire period
+
+
+# the greatest increase in profits (date and amount) over entire period
+
+
+
+# the greatest decrease in losses (date and amount) over entire period
+
+
+
